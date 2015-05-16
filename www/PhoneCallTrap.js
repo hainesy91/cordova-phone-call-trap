f5e2f0173451cd7912aed0c2d5aa44b55566569a
rammere <rm.mere@gmail.com>
2015-05-16 13:30:08 +0300

    Adding endCall Action

--- www/PhoneCallTrap.js
+++ www/PhoneCallTrap.js
@@ -3,7 +3,10 @@ var PhoneCallTrap = {
         errorCallback = errorCallback || this.errorCallback;
         cordova.exec(successCallback, errorCallback, 'PhoneCallTrap', 'onCall', []);
     },
-
+   endCall: function(successCallback, errorCallback) {
+        errorCallback = errorCallback || this.errorCallback;
+        cordova.exec(successCallback, errorCallback, 'PhoneCallTrap', 'endCall', []);
+    },
     errorCallback: function() {
         console.log("WARNING: PhoneCallTrap errorCallback not implemented");
     }
